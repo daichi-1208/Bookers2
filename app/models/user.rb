283ac5b99bc44b4fb30b902cb29a933c4a34dc5e
鈴木大地 <daichi@suzukidaichinoMacBook-Pro-2.local>
2019-12-18 19:21:10 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
+
+  def email_required?
+  	false
+  end
+  
 end
